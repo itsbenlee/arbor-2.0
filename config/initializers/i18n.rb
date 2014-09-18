f52a7c00e76a2033dc099ab15b92037f7d4479f2
Oscar Siniscalchi <oscar.siniscalchi@neonroots.com>
2014-09-18 12:29:14 -0300

    Reek offences cleaned.

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,5 +1,5 @@
 if Rails.env.development? || Rails.env.test?
-  I18n.exception_handler = lambda do |exception, locale, key, options|
-    raise "missing translation: #{ key }"
+  I18n.exception_handler = lambda do |_exception, _locale, key, _options|
+    fail "missing translation: #{ key }"
   end
 end
