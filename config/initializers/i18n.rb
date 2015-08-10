88c2aa4ca9e2d1895f5e54df588ca7fea97f415d
Pablo Gonzaga <pgonzaga.uy@gmail.com>
2015-08-10 18:07:32 -0300

    Create devise views without styles and fix code_analysis warnings
    Fix some code analysis suggestions
    
    Change md file
    
    Fix code analysis

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,5 +1,5 @@
 if Rails.env.development? || Rails.env.test?
   I18n.exception_handler = lambda do |_exception, _locale, key, _options|
-    fail "missing translation: #{ key }"
+    fail "missing translation: #{key}"
   end
 end
