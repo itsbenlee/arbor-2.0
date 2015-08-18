bcea66dcbbe44119e4edad831a2b115fdde8f581
Rosina Nabazas <rosinanabazas@gmail.com>
2015-08-18 18:19:17 -0300

    Fix tests

--- spec/features/project/canvas/completed_percentage_spec.rb
+++ spec/features/project/canvas/completed_percentage_spec.rb
@@ -10,21 +10,21 @@ feature 'Change complete percentage' do
     create :canvas, :uncomplete, { project: @project }
     sign_in @member
     visit project_canvas_path(@project)
-    expect(find('.percentage h2')).to have_text('0%')
+    expect(find('.percentage span')).to have_text('0%')
   end
 
   scenario 'should be 88% for a canvas with 8 number of answered questions' do
     create :canvas, :without_problems, { project: @project }
     sign_in @member
     visit project_canvas_path(@project)
-    expect(find('.percentage h2')).to have_text('88%')
+    expect(find('.percentage span')).to have_text('88%')
   end
 
   scenario 'should show 100% percentage for a new canvas' do
     create :canvas, { project: @project }
     sign_in @member
     visit project_canvas_path(@project)
-    expect(find('.percentage h2')).to have_text('100%')
+    expect(find('.percentage span')).to have_text('100%')
   end
 
   scenario 'should be 11% for a canvas with 1 number of answered questions' do
@@ -32,6 +32,6 @@ feature 'Change complete percentage' do
     visit project_canvas_path(@project)
     find('.problems-field textarea').set('My problem proposal')
     click_button 'save-canvas'
-    expect(find('.percentage h2')).to have_text('11%')
+    expect(find('.percentage span')).to have_text('11%')
   end
 end
