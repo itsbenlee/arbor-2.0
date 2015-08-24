4e27fbbdb170d480a9e7248f18c56db324b6c459
Bocha <jp.troncoso@neonroots.com>
2015-08-24 17:00:19 -0300

    Canvas form refinement. - Adjust textarea font-size. - Add textarea focus styles. Increase texarea height and add focus transition. - Refactor texarea related styles in canvas.scss. - Add hover transition for canvas items. - Adjust textarea show, hide and resize functionality. Refactor related JS file.

--- app/assets/javascripts/canvases/init.js
+++ app/assets/javascripts/canvases/init.js
@@ -1,13 +1,28 @@
 ARBOR.canvases.init = function() {
-  var $canvasAttributeLinks = $('li.canvas-item');
-  var $canvasAttributeFields = $('.canvas-fields');
+  var $canvasLinks    = $('li.canvas-item'),
+      $canvasFields   = $('.canvas-fields'),
+      $canvasTextarea = $('.canvas-fields textarea');
+
+  $canvasTextarea.each(function(array) {
+    $(this).height($('textarea')[array].scrollHeight - 16);
+
+    var offset = this.offsetHeight - this.clientHeight;
+    var resizeTextarea = function(el) {
+        $(el).css('height', 'auto').css('height', el.scrollHeight + offset);
+    };
+
+    $(this).on('keyup input', function() { resizeTextarea(this); });
+  });
+
+  $canvasFields.not(':eq(0)').hide();
+  $canvasFields.css('opacity', 1);
 
   function showAttribute(type) {
     $('.' + type + '-field.canvas-fields').show();
   }
 
   function setVisited(type) {
-    $.each($canvasAttributeLinks, function(index, canvasItem) {
+    $canvasLins.each(function(index, canvasItem) {
       var item = $(this);
       if(item.attr('type') == type) {
         item.addClass('canvas-visited');
@@ -17,8 +32,8 @@ ARBOR.canvases.init = function() {
     });
   }
 
-  $canvasAttributeLinks.click(function() {
-    $canvasAttributeFields.hide();
+  $canvasLinks.click(function() {
+    $canvasFields.hide();
     var type = $(this).attr('type');
     showAttribute(type);
     setVisited(type);
