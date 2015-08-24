4d8d26e605fcb522be34a505826ffb188ee89833
vicocas <vicocas@gmail.com>
2015-08-24 18:06:10 -0300

    Configuration and initial styling.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,4 +8,6 @@ Railsroot::Application.configure do
   config.active_support.deprecation = :log
   config.active_record.migration_error = :page_load
   config.assets.debug = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,6 +1,5 @@
 Devise.setup do |config|
-  config.mailer_sender =
-    'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = ENV['FROM_EMAIL_ADDRESS']
   require 'devise/orm/active_record'
   config.case_insensitive_keys = [:email]
   config.strip_whitespace_keys = [:email]
