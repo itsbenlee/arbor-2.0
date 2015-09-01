8adf6fb6834a53284b6a6765b5183c65a7770024
vicocas <vicocas@gmail.com>
2015-09-01 14:49:14 -0300

    Fix bug and add tests.
    Allign hashes.
    
    Remove Trailing whitespace.

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,6 +1,6 @@
 class Invite < ActiveRecord::Base
-  validates_presence_of :email
-  validates_presence_of :project
+  validates_presence_of :email, :project
+  validates_uniqueness_of :email, scope: :project_id
 
   belongs_to :project
 end
--- spec/models/invite_spec.rb
+++ spec/models/invite_spec.rb
@@ -1,9 +1,27 @@
 require 'spec_helper'
 
 describe Invite do
-  let(:invite) { create :invite }
+  let(:project) { create :project }
+  let!(:invite) { create :invite, email: 'test@test.com', project: project }
 
   it { should validate_presence_of :email }
   it { should validate_presence_of :project }
+  it { should validate_uniqueness_of(:email).scoped_to(:project_id)}
   it { should belong_to :project }
+
+  it 'must not accept duplicate invites' do
+    expect{ create :invite, email: 'test@test.com', project: project }
+      .to raise_error(ActiveRecord::RecordInvalid)
+  end
+
+  it 'must accept same email and different project' do
+    project2 = create :project
+    expect{ create :invite, email: 'test@test.com', project: project2 }
+      .not_to raise_error
+  end
+
+  it 'must accept same project and different email' do
+    expect{ create :invite, email: 'test2@test2.com', project: project }
+      .not_to raise_error
+  end
 end
