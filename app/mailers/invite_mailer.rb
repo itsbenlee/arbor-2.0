048ec8dbdec10c7e56a6f72a63ece99652d546ec
Rosina Nabazas <rosinanabazas@gmail.com>
2015-09-01 16:16:49 -0300

    Add mail inline styles, correct  image and link paths

--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -5,6 +5,9 @@ class InviteMailer < ActionMailer::Base
     @project_name = data[:project_name]
     @inviter = data[:inviter]
     @is_new = is_new
+    @link_url = root_url
+
+    attachments.inline['logo.svg'] = File.read('public/arbor-logo.svg')
 
     mail to: data[:email], subject: t('mailer.invite.subject')
   end
