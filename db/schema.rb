55e947ef09fa9460a059183713fc94b342937d9a
Rosina Nabazas <rosinanabazas@gmail.com>
2015-09-03 11:57:36 -0300

    Backlog User Story List
    Add section's stylesheet, top-nav bar
    Add new top-nav link and correct internationalization text
    Add section's general paddings
    Style user stories texts, separate them into spans
    Resolve conflict
    Correct old classes changed on conflict
    Correct css warnings

--- db/schema.rb
+++ db/schema.rb
@@ -61,7 +61,7 @@ ActiveRecord::Schema.define(version: 20150902182239) do
   add_index "goals", ["hypothesis_id"], name: "index_goals_on_hypothesis_id", using: :btree
 
   create_table "hypotheses", force: :cascade do |t|
-    t.string   "description"
+    t.string   "description",        limit: 255
     t.integer  "project_id"
     t.integer  "hypothesis_type_id"
     t.integer  "order"
@@ -73,8 +73,8 @@ ActiveRecord::Schema.define(version: 20150902182239) do
   add_index "hypotheses", ["project_id"], name: "index_hypotheses_on_project_id", using: :btree
 
   create_table "hypothesis_types", force: :cascade do |t|
-    t.string   "code"
-    t.string   "description"
+    t.string   "code",        limit: 255
+    t.string   "description", limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -103,7 +103,6 @@ ActiveRecord::Schema.define(version: 20150902182239) do
     t.datetime "updated_at"
   end
 
-  add_index "projects", ["name"], name: "index_projects_on_name", using: :btree
   add_index "projects", ["owner_id"], name: "index_projects_on_owner_id", using: :btree
 
   create_table "user_stories", force: :cascade do |t|
@@ -126,20 +125,20 @@ ActiveRecord::Schema.define(version: 20150902182239) do
   add_index "user_stories", ["story_number"], name: "index_user_stories_on_story_number", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
-    t.string   "reset_password_token"
+    t.string   "email",                  limit: 255, default: "",    null: false
+    t.string   "encrypted_password",     limit: 255, default: "",    null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,     null: false
+    t.integer  "sign_in_count",                      default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                             null: false
-    t.datetime "updated_at",                             null: false
-    t.string   "full_name"
-    t.boolean  "admin",                  default: false
+    t.datetime "created_at",                                         null: false
+    t.datetime "updated_at",                                         null: false
+    t.string   "full_name",              limit: 255
+    t.boolean  "admin",                              default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
