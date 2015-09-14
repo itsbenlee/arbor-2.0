e80086f6be544a638d28e9cc753a9e34af45435d
Rosina Nabazas <rosinanabazas@gmail.com>
2015-09-14 16:49:30 -0300

    Style signout link/icon
    Add signout svg icon, and link to backlog view
    Add Signout icon to Lab view
    Add top-nav link to Canvas view
    Fix tests, delete link from sidebar button

--- spec/features/user/log_out_spec.rb
+++ spec/features/user/log_out_spec.rb
@@ -1,17 +1,17 @@
 require 'spec_helper'
 
-feature 'Sign in' do
+feature 'Sign in', js: true do
   context 'when the user logs in' do
     background do
       sign_in create :user
     end
 
     scenario 'should show the logout button' do
-      expect(page).to have_css('#sidebar a.logout')
+      expect(page).to have_css('#top-nav a.logout ')
     end
 
     scenario 'clicking the logout should sign the user out' do
-      find('#sidebar a.logout').click
+      find('#top-nav a.logout').click
       expect(page).to have_content('Signed out successfully')
     end
   end
