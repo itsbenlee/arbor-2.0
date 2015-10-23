0d1f2085ff0a351975b59b89c392e8c117c2ce0f
Patrick Schulze <pschulze0408@googlemail.com>
2015-10-23 15:31:28 +0200

    Fixed the error of being redirected to an unstyled view after inputing a too short password when signing up.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,8 +7,33 @@ class RegistrationsController < Devise::RegistrationsController
     redirect_to new_user_session_path
   end
 
-  def create
-    super
+  def create # rubocop:disable all
+    build_resource(sign_up_params)
+
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      if resource.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_flashing_format?
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        set_flash_message(
+          :notice,
+          "signed_up_but_#{resource.inactive_message}".to_sym
+        ) if is_flashing_format?
+        expire_data_after_sign_in!
+        respond_with(
+          resource,
+          location: after_inactive_sign_up_path_for(resource)
+        )
+      end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      resource.errors.full_messages.each { |message| flash[:alert] = message }
+      respond_with resource, location: new_user_session_path
+    end
   end
 
   private
