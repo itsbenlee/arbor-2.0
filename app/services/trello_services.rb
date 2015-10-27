d8135565bc4cfdd54397144c639039ce9620b494
vicocas <vicocas@gmail.com>
2015-10-27 21:57:13 -0200

    Expand trello export.
    refactor methods
    
    remove old method.
    
    update gem and Trello API syntax

--- app/services/trello_services.rb
+++ app/services/trello_services.rb
@@ -13,17 +13,40 @@ class TrelloServices
       starred: false
     }
 
-    board = Trello::Board.create(fields)
-    board.lists.each(&:close!)
-    list = Trello::List.create(
-      name: I18n.t('trello.default_list'), board_id: board.id)
+    @board = Trello::Board.create(fields)
+    @board.lists.each(&:close!)
+    new_list = Trello::List.create(
+      name: I18n.t('trello.default_list'), board_id: @board.id)
 
-    @project.user_stories.each do |user_story|
-      Trello::Card.create(name: user_story.log_description, list_id: list.id)
-    end
+    export_stories(new_list)
   end
 
   private
+
+  def export_stories(list)
+    @project.user_stories.each do |user_story|
+      new_card_name =
+        "[#{user_story.story_number}] "\
+        "(#{user_story.estimated_points}) #{user_story.log_description}"
+      new_card = Trello::Card.create(name: new_card_name, list_id: list.id)
+
+      create_acceptance_criterions_checklist(user_story, new_card)
+      create_constraints_checklist(user_story, new_card)
+    end
+  end
+
+  %w(acceptance_criterions constraints).each do |type|
+    define_method("create_#{type}_checklist") do |user_story, new_card|
+      checklist = Trello::Checklist.create(
+        name: I18n.t("backlog.#{type}"),
+        board_id: @board.id,
+        card_id: new_card.id)
+
+      user_story.send(type.to_sym).each do |element|
+        checklist.add_item(element.description)
+      end
+    end
+  end
 
   def config
     Trello.configure do |config|
