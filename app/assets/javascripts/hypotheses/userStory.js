068313140928de77279d5067d3d205aba1c703ae
AlejandroFernandesAntunes <alejandro.fernandes@neonroots.com>
2015-11-02 18:37:00 -0300

    Added js function to resize inputs
    Adressed suggestions

--- app/assets/javascripts/hypotheses/userStory.js
+++ app/assets/javascripts/hypotheses/userStory.js
@@ -87,6 +87,11 @@ function UserStory() {
 
         bindUserStoriesSortEvent();
 
+        //iterate over actions and set the size of the containers to fit their content
+        var elementsThatNeedWidthFixes = ['.action#user_story_action', '.result#user_story_result'];
+        fixInputWidths(elementsThatNeedWidthFixes);
+
+
         var $userStoryForm = $('#edit_user_story_' + userStoryId);
         $appContent.scrollTo($userStoryForm, 200);
     });
@@ -128,4 +133,27 @@ function UserStory() {
 
     return false;
   });
+}
+
+// this function receives an array of class#id of elements that need to be width-fixed, Ale
+function fixInputWidths(elementsClassIds) {
+  for (var i in elementsClassIds) {
+    $(elementsClassIds[i]).each(function(i, obj) {
+      var width = stringWidthInPixels(obj.value, this.id);
+      obj.style.minWidth = width + 'px';
+    });
+  }
+}
+// width calculator considering font used, and adding 3 px in case italic, Ale
+function stringWidthInPixels(string, elementId) {
+  // using an auxiliary canvas I can measure the text
+  var actualFont = window.getComputedStyle(document.getElementById(elementId)).font;
+  var canvas = document.createElement('canvas');
+  var ctx = canvas.getContext('2d');
+  ctx.font = actualFont;
+  //just in case we add it 3 pxs.
+  var width = ctx.measureText(string).width + 3;
+
+  canvas = null;
+  return width;
 }
