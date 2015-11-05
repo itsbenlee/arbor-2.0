d07c02445839a5b31906fceca33346f7ac1dee51
AlejandroFernandesAntunes <alejandro.fernandes@neonroots.com>
2015-11-05 11:25:21 -0300

    Changed question to find out if Im on Safari

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,4 +19,15 @@
 //= require_tree ./vendor
 //= require_tree ./attachments
 
-var is_safari = navigator.userAgent.indexOf('Safari') > -1;
+
+/*
+
+Only on Safari this kind of constructor exists
+
+An unique naming pattern in its naming of constructors.
+This is the least durable method of all listed properties, because it's undocumented.
+On the other hand, there's no benefit in renaming the constructor,
+so it's likely to stay for a long while.
+
+*/
+var is_safari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
