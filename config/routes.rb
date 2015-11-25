a73f7f7a3bf3c8bda0fdd5121f95b56e5618b8db
Damian Cardozo <damian.cardozo@neonroots.com>
2015-11-25 12:16:01 -0300

    Create new layout.
    New nav structure and links
    Styles and responsivness.
    Fix code smells

--- config/routes.rb
+++ config/routes.rb
@@ -98,5 +98,61 @@ Railsroot::Application.routes.draw do
 
   namespace :arbor_reloaded do
     root to: 'projects#index'
+
+    resources :projects, shallow: true do
+      get 'log', on: :member
+      resources :lab,
+        only: [:index, :create, :update],
+        as: :hypotheses,
+        controller: :hypotheses
+      resources :canvas,
+        only: [:index, :create],
+        as: :canvases,
+        controller: :canvases
+      put 'user_stories/order',
+          controller: :projects,
+          action: :order_stories,
+          as: :reorder_backlog
+
+      resources :backlog,
+          except: [:show, :new],
+          as: :user_stories,
+          controller: :user_stories do
+        resources :acceptance_criterions, only: [:create, :update]
+        resources :constraints, only: [:create, :update]
+        resources :tags, only: [:create, :index]
+        resources :comments, only: [:create]
+      end
+
+      get 'user_stories/export',
+        controller: :user_stories,
+        action: :export,
+        as: :backlog_export
+
+      get 'tags/filter', controller: :tags, action: :filter
+      get 'tags/index', controller: :tags, action: :index
+      get 'tag/delete', controller: :tags, action: :delete
+
+      put 'hypotheses/order', controller: :hypotheses, action: :update_order
+      get 'hypotheses/export', controller: :hypotheses, action: :export
+      get 'hypotheses/export/trello',
+        controller: :hypotheses,
+        action: :export_to_trello,
+        as: :trello_export
+
+      put 'hypotheses/user_stories/order',
+        controller: :user_stories,
+        action: :update_order,
+        as: :user_stories_order
+
+      get '/list_backlog', controller: :projects, action: :backlog
+      resources :attachments, only: [:index, :create, :destroy]
+
+      post '/copy', controller: :projects, action: :copy
+
+      delete 'remove_member_from_project',
+        controller: :projects,
+        action: :remove_member_from_project
+    end
   end
 end
