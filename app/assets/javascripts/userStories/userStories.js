15533510dad63f34ec6a7a33b2446b4f88b776ae
AlejandroFernandesAntunes <alejandro.fernandes@neonroots.com>
2015-12-01 19:45:58 -0300

    Change archive link to backlog view
    Fixes
    
    Fixes

--- app/assets/javascripts/userStories/userStories.js
+++ app/assets/javascripts/userStories/userStories.js
@@ -142,6 +142,7 @@ function UserStories() {
       refreshStories();
       bindAcceptanceCriterionFadeOut();
       bindConstraintFadeOut();
+      bindArchiveLink();
     });
   }
 
@@ -424,6 +425,19 @@ function UserStories() {
     });
   }
 
+  function bindArchiveLink() {
+    $('.archive-link').click(function() {
+      var userStory = {
+            user_story: { archived: true }
+          }
+          url = $(this).data('url'),
+          type  = 'put';
+
+      editFormAjax(url, type, userStory);
+      return false;
+    });
+  }
+
   function bindEditConstraint() {
     $editConstraintForm = $('.edit_constraint');
 
@@ -450,7 +464,7 @@ function UserStories() {
   }
 
   function refreshStories() {
-    $('.user-story-edit-form .user-story-input input:text').each(function() {
+    $('.user-story-edit-form .user-story-input input.role:text').each(function() {
       var $this  = $(this),
           $span  = $this.parent().find('span'),
           $value = $this.val;
