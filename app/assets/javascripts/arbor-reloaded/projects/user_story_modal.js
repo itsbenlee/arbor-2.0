4a7b4fb9bd37eed50479f062ebfbfd59a2bf60d5
vicocas <vicocas@gmail.com>
2016-01-11 12:23:48 -0300

    estimate story and test
    progreess on estimate story.
    
    Finish estimation story feature.
    
    fixes on estimation.

--- app/assets/javascripts/arbor-reloaded/projects/user_story_modal.js
+++ app/assets/javascripts/arbor-reloaded/projects/user_story_modal.js
@@ -44,6 +44,22 @@ function changeFibonnacciEstimation() {
 
   $dropdown.change(function(event) {
     $estimationPoints.html(this.value);
+    $.ajax({
+      url: $(this).data('url'),
+      dataType: 'json',
+      method: 'PUT',
+      data: { user_story: { estimated_points: this.value } },
+      success: function (response) {
+        if(response.success) {
+          if (response.data.estimated_points !== null)
+            $('.backlog-user-story[data-id='+ response.data.id +']')
+            .find('.story-points').text(response.data.estimated_points);
+          else {
+            $('.backlog-user-story[data-id='+ response.data.id +']').find('.story-points').text('?')
+          }
+        }
+      }
+    });
   });
 }
 
