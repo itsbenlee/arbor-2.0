8c7cee18b81111feef84c247c5e11a6915ac7577
AlejandroFernandesAntunes <alejandro.fernandes@neonroots.com>
2016-01-19 09:33:29 -0300

    Adressed suggestions

--- spec/controllers/api_slack/user_stories_controller_spec.rb
+++ spec/controllers/api_slack/user_stories_controller_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe ApiSlack::UserStoriesController do
         channel_id: '23456'
 
       hash_response = JSON.parse(response.body)
-      expect(hash_response['success']).to eq(true)
+      expect(hash_response['success']).to be true
       expect(Project.find(project.id).user_stories.last.role).to eq('Admin')
       expect(Project.find(project.id).user_stories.last.action).to eq('to have privileges to all the database')
       expect(Project.find(project.id).user_stories.last.result).to eq('so that I can check the tables.')
@@ -29,7 +29,7 @@ RSpec.describe ApiSlack::UserStoriesController do
         channel_id: '23456'
 
       hash_response = JSON.parse(response.body)
-      expect(hash_response['success']).to eq(false)
+      expect(hash_response['success']).to be false
       expect(hash_response['errors']).to have_content('Missing Role')
     end
 
@@ -42,7 +42,7 @@ RSpec.describe ApiSlack::UserStoriesController do
 
 
       hash_response = JSON.parse(response.body)
-      expect(hash_response['success']).to eq(false)
+      expect(hash_response['success']).to be false
       expect(hash_response['errors']).to have_content('channel_id is missing')
     end
 
@@ -54,7 +54,7 @@ RSpec.describe ApiSlack::UserStoriesController do
         channel_id: '23456'
 
       hash_response = JSON.parse(response.body)
-      expect(hash_response['success']).to eq(false)
+      expect(hash_response['success']).to be false
       expect(hash_response['errors']).to have_content('user_id is missing')
     end
 
@@ -68,7 +68,7 @@ RSpec.describe ApiSlack::UserStoriesController do
         channel_id: '23456'
 
       hash_response = JSON.parse(response.body)
-      expect(hash_response['success']).to eq(false)
+      expect(hash_response['success']).to be false
       expect(hash_response['errors']).to have_content("User doesn't have permissions on project")
     end
   end
