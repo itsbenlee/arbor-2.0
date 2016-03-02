a6eefc86f997476b82bcf75fa10b658c1ae1abb8
Rosina Nabazas <rosinanabazas@gmail.com>
2016-03-02 13:49:34 -0300

    Apply changes on Create user story container
    Fix classes names
    Delete wrong code paste
    Refactor js

---
+++ app/assets/javascripts/arbor-reloaded/user_stories/check_if_filled.js
@@ -0,0 +1,20 @@
+changeButtonColor();
+
+function changeButtonColor() {
+  var $emptyInput = $('.backlog-story-input input'),
+      $role = $('#role-input'),
+      $action = $('#action-input'),
+      $result = $('#result-input');
+
+  $(this).keyup(function() {
+    count = $emptyInput.filter(function() {
+      return $(this).val().length > 0;
+    }).length;
+
+    if (count === 3) {
+      $('.create-btn').addClass('success-button');
+    } else {
+        $('.create-btn').removeClass('success-button');
+    }
+  });
+}
