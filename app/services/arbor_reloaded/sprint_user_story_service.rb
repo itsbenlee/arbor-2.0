095de6b113daa9a59d75f1c27d3867226d25e714
pgonzaga2012 <pgonzaga.uy@gmail.com>
2017-06-22 12:27:55 -0300

    Update story status inside a sprint

--- app/services/arbor_reloaded/sprint_user_story_service.rb
+++ app/services/arbor_reloaded/sprint_user_story_service.rb
@@ -18,5 +18,20 @@ module ArborReloaded
 
       sprint_user_story.destroy
     end
+
+    def update_story_status(status = nil)
+      delete_user_story and return unless status
+
+      unless SprintUserStory::STATUS.include? status
+        raise WrongUserStoryStatusError
+      end
+
+      sprint_user_story = SprintUserStory.find_or_create_by(
+        user_story_id: @user_story.id,
+        sprint_id: @sprint.id
+      )
+
+      sprint_user_story.update(status: status)
+    end
   end
 end
--- spec/services/arbor_reloaded/sprint_user_story_service_spec.rb
+++ spec/services/arbor_reloaded/sprint_user_story_service_spec.rb
@@ -3,6 +3,14 @@ describe ArborReloaded::SprintUserStoryService do
   let!(:project)   { create :project, owner: user }
   let(:user_story) { create :user_story, project: project }
   let(:sprint)     { create :sprint, project: project }
+  let(:sprint_user_story_service) do
+    ArborReloaded::SprintUserStoryService.new(
+      user,
+      project.id,
+      sprint.id,
+      user_story.id
+    )
+  end
 
   describe '.new' do
     describe "project doesn't belongs to user" do
@@ -45,15 +53,6 @@ describe ArborReloaded::SprintUserStoryService do
   end
 
   describe '.delete_user_story' do
-    let(:sprint_user_story_service) do
-      ArborReloaded::SprintUserStoryService.new(
-        user,
-        project.id,
-        sprint.id,
-        user_story.id
-      )
-    end
-
     describe "user story doesn't belongs to sprint" do
       it 'should return an ActiveRecord::RecordNotFound error' do
         expect { sprint_user_story_service.delete_user_story }.to raise_error ActiveRecord::RecordNotFound
@@ -67,6 +66,56 @@ describe ArborReloaded::SprintUserStoryService do
         expect {
           sprint_user_story_service.delete_user_story
         }.to change { sprint.user_stories.count }.from(1).to(0)
+      end
+    end
+  end
+
+  describe '.update_story_status' do
+    describe 'remove from sprint if status is nil' do
+      let!(:sprint_user_story) { create :sprint_user_story, user_story: user_story, sprint: sprint }
+
+      it 'removes the story' do
+        expect {
+          sprint_user_story_service.update_story_status
+        }.to change { sprint.user_stories.count }.from(1).to(0)
+      end
+    end
+
+    describe 'status is not allowed' do
+      it 'should raise a WrongUserStoryStatusError' do
+        expect {
+          sprint_user_story_service.update_story_status('fake status')
+        }.to raise_error WrongUserStoryStatusError
+      end
+    end
+
+    describe 'status is allowed and story is not in sprint' do
+      it 'adds the story' do
+        expect {
+          sprint_user_story_service.update_story_status('WIP')
+        }.to change { sprint.user_stories.count }.from(0).to(1)
+      end
+
+      it 'updates the status to the right one' do
+        sprint_user_story_service.update_story_status('WIP')
+
+        expect(SprintUserStory.last.status).to eq 'WIP'
+      end
+    end
+
+    describe 'status is allowed and story belongs to sprint' do
+      let!(:sprint_user_story) { create :sprint_user_story, user_story: user_story, sprint: sprint }
+
+      it 'just update the status' do
+        expect {
+          sprint_user_story_service.update_story_status('WIP')
+        }.not_to change { sprint.user_stories.count }
+      end
+
+      it 'changes the status to WIP' do
+        expect {
+          sprint_user_story_service.update_story_status('WIP')
+        }.to change { SprintUserStory.last.status }.from('PLANNED').to('WIP')
       end
     end
   end
