24e8ca76d9db115dd92719891b337f0126f28050
Pablo Gonzaga <pablo.gonzaga@neonroots.com>
2017-06-26 17:08:34 -0300

    Story/950/add start date to project (#783)
    * Add starting date and update rest api endpoint
    
    * Fix code offenses

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170620171843) do
+ActiveRecord::Schema.define(version: 20170626172344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -189,6 +189,7 @@ ActiveRecord::Schema.define(version: 20170620171843) do
     t.integer  "team_id"
     t.string   "slack_iw_url"
     t.boolean  "slack_enabled",                 default: false
+    t.date     "starting_date"
   end
 
   add_index "projects", ["name"], name: "index_projects_on_name", using: :btree
--- spec/controllers/arbor_reloaded/api/v1/projects_controller_spec.rb
+++ spec/controllers/arbor_reloaded/api/v1/projects_controller_spec.rb
@@ -1,30 +1,29 @@
 require 'spec_helper'
 
 RSpec.describe ArborReloaded::Api::V1::ProjectsController do
+  let(:user)         { create :user }
+  let(:project_name) { Faker::App.name }
+  let(:access_token) { ActionController::HttpAuthentication::Token.encode_credentials(user.access_token) }
+
   describe 'without user' do
     describe '#create' do
+      subject { response }
+
       before do
         request.env['HTTP_AUTHORIZATION'] =
           ActionController::HttpAuthentication::Token.encode_credentials('fake credentials')
         post :create
       end
 
-      it 'should return an unauthorization code' do
-        expect(response.code.to_i).to eq(401)
-      end
+      it { should  have_http_status 401 }
     end
   end
 
   describe 'for common user' do
-    let(:user) { create :user }
-
     describe '#create' do
       before do
-        request.env['HTTP_AUTHORIZATION'] =
-          ActionController::HttpAuthentication::Token.encode_credentials(user.access_token)
-
-        @project_name = Faker::App.name
-        post :create, { project: { name: @project_name } }
+        request.env['HTTP_AUTHORIZATION'] = access_token
+        post :create, { project: { name: project_name } }
       end
 
       it 'should return a success code' do
@@ -33,18 +32,16 @@ RSpec.describe ArborReloaded::Api::V1::ProjectsController do
 
       it 'should return the right project name' do
         response_hash = JSON.parse(response.body)
-        expect(response_hash['name']).to eq(@project_name)
+        expect(response_hash['name']).to eq(project_name)
       end
     end
 
     describe '#duplicated_create' do
       before do
-        request.env['HTTP_AUTHORIZATION'] =
-          ActionController::HttpAuthentication::Token.encode_credentials(user.access_token)
+        request.env['HTTP_AUTHORIZATION'] = access_token
 
-        @project_name = Faker::App.name
-        post :create, { project: { name: @project_name } }
-        post :create, { project: { name: @project_name } }
+        post :create, { project: { name: project_name } }
+        post :create, { project: { name: project_name } }
       end
 
       it 'should return a success code' do
@@ -53,8 +50,21 @@ RSpec.describe ArborReloaded::Api::V1::ProjectsController do
 
       it 'should return the right project name' do
         response_hash = JSON.parse(response.body)
-        expect(response_hash['name']).to eq(@project_name)
+        expect(response_hash['name']).to eq(project_name)
       end
     end
+  end
+
+  describe 'for project with starting date' do
+    let(:project_starting_date) { 1.day.from_now }
+    subject { JSON.parse response.body }
+
+    before do
+      request.env['HTTP_AUTHORIZATION'] = access_token
+      post :create, { project: { name: project_name, starting_date: project_starting_date} }
+    end
+
+    it { should include('name' => project_name)}
+    it { should include('starting_date' => project_starting_date.to_date.to_s)}
   end
 end
