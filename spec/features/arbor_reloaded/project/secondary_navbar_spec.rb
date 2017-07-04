f79fa3ab273acdeb4e812fcaa6d51a2c29cd0b7d
Pablo Gonzaga <pgonzaga.uy@gmail.com>
2017-07-04 11:02:42 -0300

    Add access denied test

--- spec/features/arbor_reloaded/project/secondary_navbar_spec.rb
+++ spec/features/arbor_reloaded/project/secondary_navbar_spec.rb
@@ -5,20 +5,33 @@ feature 'secondary navbar' do
   let(:project) { create :project, owner: user }
   subject { page }
 
-  background do
-    sign_in user
-    visit arbor_reloaded_project_user_stories_path(project.id)
-  end
-
-  context 'release plan link is present' do
-    scenario { should have_selector(:link_or_button, 'Release plan') }
-  end
-
-  context 'release plan link works' do
+  context 'user is the project owner' do
     background do
-      click_link 'Release plan'
+      sign_in user
+      visit arbor_reloaded_project_user_stories_path(project.id)
     end
 
-    scenario { should have_current_path release_plan_arbor_reloaded_project_path(project)  }
+    context 'release plan link is present' do
+      scenario { should have_selector(:link_or_button, 'Release plan') }
+    end
+
+    context 'release plan link works' do
+      background do
+        click_link 'Release plan'
+      end
+
+      scenario { should have_current_path release_plan_arbor_reloaded_project_path(project)  }
+    end
+  end
+
+  context "user isn't a team member" do
+    let(:other_user) { create :user }
+
+    background do
+      sign_in other_user
+      visit arbor_reloaded_api_v1_project_release_plans_path(project.id)
+    end
+
+    scenario { should have_text 'HTTP Token: Access denied.' }
   end
 end
