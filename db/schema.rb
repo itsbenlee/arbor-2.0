5f19ba361f3234523d53e013f2326d5e6af87731
Pablo Gonzaga <pgonzaga.uy@gmail.com>
2017-07-05 11:23:39 -0300

    Fix schema

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 20170705135531) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "pg_stat_statements"
 
   create_table "acceptance_criterions", force: :cascade do |t|
     t.text     "description"
@@ -111,7 +110,6 @@ ActiveRecord::Schema.define(version: 20170705135531) do
     t.integer  "user_story_id",             null: false
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
-    t.integer  "order"
   end
 
   add_index "constraints", ["description"], name: "index_constraints_on_description", using: :btree
@@ -262,7 +260,6 @@ ActiveRecord::Schema.define(version: 20170705135531) do
     t.integer  "hypothesis_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "order"
     t.integer  "story_number"
     t.integer  "backlog_order"
     t.boolean  "archived",                     default: false
@@ -293,8 +290,8 @@ ActiveRecord::Schema.define(version: 20170705135531) do
     t.boolean  "admin",                  default: false
     t.string   "slack_id"
     t.string   "avatar"
-    t.string   "trello_token"
     t.string   "slack_auth_token"
+    t.string   "trello_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
