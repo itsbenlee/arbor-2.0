1b1a54129d94c5ce5922765c273d777416a960a8
Pablo Gonzaga <pgonzaga.uy@gmail.com>
2017-09-28 13:44:22 -0300

    Remove arrow event on user stories modal

--- app/assets/javascripts/arbor-reloaded/projects/user_story_modal.js
+++ app/assets/javascripts/arbor-reloaded/projects/user_story_modal.js
@@ -2,29 +2,6 @@ function UserStory() {
   var $storyModal     = $('#story-detail-modal'),
       $storyActions   = $storyModal.find('.story-actions');
 
-  function NavigateUserStories() {
-    $(document).unbind('keydown');
-    $(document).bind('keydown', function(e) {
-      var $inEditMode = $('#edit-mode').hasClass('active');
-
-      if (!$inEditMode) {
-        var $nextStory = $storyModal.find('.icn-arrow-right'),
-            $prevStory = $storyModal.find('.icn-arrow-left');
-
-        switch (e.which) {
-          case 37:
-            $prevStory.trigger('click');
-            break;
-          case 39:
-            $nextStory.trigger('click');
-            break;
-        }
-      }
-    });
-  }//navigate user stories
-
-  NavigateUserStories();
-
   $storyModal.on('close.fndtn.reveal', function() {
     $(document).unbind('keydown');
   });
